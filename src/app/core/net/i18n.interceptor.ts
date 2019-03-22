dabfe3be37aad10546bd7fbe7b778db5fe6e7a6a
cenbiq <915510541@qq.com>
2019-03-22 11:47:54 +0800

    修改登录跳转方式

--- src/app/core/net/i18n.interceptor.ts
+++ src/app/core/net/i18n.interceptor.ts
@@ -6,14 +6,16 @@ import { I18nService } from '..';
 @Injectable()
 export class I18nInterceptor implements HttpInterceptor {
 
-    constructor(private i18n: I18nService) {}
+    constructor(private i18n: I18nService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const obj = {};
-        obj['lang'] = this.i18n.currentLang; // this.settings.lang || ''; // 出现 null 值将导致错误
-        req = req.clone({
-            setHeaders: obj
-        });
+        if (!req.params.has('_allow_no_language')) {
+            const obj = {};
+            obj['lang'] = this.i18n.currentLang; // this.settings.lang || ''; // 出现 null 值将导致错误
+            req = req.clone({
+                setHeaders: obj
+            });
+        }
         return next.handle(req);
     }
 
